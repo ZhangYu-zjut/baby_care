bc996c15c031507e018c8f21a49f321d8a974a7a
ZhangYu-zjut <37924374+ZhangYu-zjut@users.noreply.github.com>
2025-04-26 23:43:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'  // 强制输出到 build 目录
+  }
 })
